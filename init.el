55f3636636689dce4f4a0ff11ccb0f99030dd606
Nicolas Chapon <nchapon@gmail.com>
2013-04-10 22:27:10 +0200

    Add pomodoro mode

--- init.el
+++ init.el
@@ -40,6 +40,7 @@
    (cons 'gitignore-mode melpa)
    (cons 'clojure-mode melpa)
    (cons 'popwin melpa)
+   (cons 'pomodoro melpa)
    (cons 'smex melpa)
    (cons 'color-theme-solarized melpa)
    (cons 'nrepl melpa)))
