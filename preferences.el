a9351fe467dc823fc9163ef454cd5f3947882574
Nicolas Chapon <nchapon@gmail.com>
2013-04-26 17:03:18 +0200

    Remove indent auto on ENTER

--- preferences.el
+++ preferences.el
@@ -62,7 +62,7 @@
 ;; Default indentation
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
-(setq indent-line-function 'insert-tab)
+;;(setq indent-line-function 'insert-tab)
 
 
 ;; Always display line and column numbers
