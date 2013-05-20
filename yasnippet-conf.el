447918d9f56a76dbe08979f6025418e151306d35
Nicolas Chapon <nchapon@gmail.com>
2013-05-20 22:11:06 +0200

    Fix orgmode conflict with Yasnippet

--- yasnippet-conf.el
+++ yasnippet-conf.el
@@ -12,5 +12,17 @@
 (setq yas/wrap-around-region t)
 
 
+;; fix org mode conflict
+;; http://orgmode.org/manual/Conflicts.html
+(defun yas/org-very-safe-expand ()
+            (let ((yas/fallback-behavior 'return-nil)) (yas/expand)))
+
+(add-hook 'org-mode-hook
+                    (lambda ()
+                      (make-variable-buffer-local 'yas/trigger-key)
+                      (setq yas/trigger-key [tab])
+                      (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
+                      (define-key yas/keymap [tab] 'yas/next-field)))
+
 
 (provide 'yasnippet-conf)
