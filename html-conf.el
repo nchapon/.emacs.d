f46b839f83f1baca2cda45b198c1def68d66bfd0
Nicolas Chapon <nchapon@gmail.com>
2013-10-11 15:31:16 +0200

    Change html close tag key binding

--- html-conf.el
+++ html-conf.el
@@ -1,18 +1,13 @@
 ;; HTML configuration mode
 
-
-
-
 (eval-after-load "sgml-mode"
   '(progn
      (define-key html-mode-map (kbd "RET") 'newline-and-indent)
-     (define-key html-mode-map (kbd "C->") 'sgml-close-tag)
+     (define-key html-mode-map (kbd "C-/") 'sgml-close-tag)
 
      (require 'tagedit)
      (tagedit-add-paredit-like-keybindings)
      (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
-
-
 
 
 (provide 'html-conf)
