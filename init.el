126cf6716675749f43769ec23e22f5811e7dcc46
Nicolas Chapon <nchapon@gmail.com>
2014-01-27 14:38:28 +0100

    Add external projects to load path

--- init.el
+++ init.el
@@ -23,6 +23,11 @@
 (add-to-list 'load-path user-settings-dir)
 
 
+;; Add external projects to load path
+(dolist (project (directory-files site-lisp-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
+
 ;;;
 ;;; Setup packages
 ;;;
