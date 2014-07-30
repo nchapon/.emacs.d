614a881e88a6173f59043649d77cf94ab219a901
Nicolas Chapon <nchapon@gmail.com>
2014-07-30 17:25:04 +0200

    Experimental Org Grep

--- nc-org.el
+++ nc-org.el
@@ -11,4 +11,12 @@
 ;; Diary
 (setq org-agenda-include-diary t)
 
+;;; Experimental org-grep
+(defun org-grep (regexp)
+  "Find in org files "
+  (interactive (list (read-regexp "Enter a regexp to grep:")))
+  (find-grep
+   (format "find ~/notes -name '*.org' -print0 | xargs -0 grep -i -n -- %s" regexp)))
+
+
 (provide 'nc-org)
