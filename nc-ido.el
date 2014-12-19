e40804b3e105128224253b64d7af0777733fedfb
Nicolas Chapon <nchapon@gmail.com>
2014-12-19 17:43:52 +0100

    sudo when necessary

--- nc-ido.el
+++ nc-ido.el
@@ -49,6 +49,15 @@
       (message "Opening file...")
     (message "Aborting")))
 
+
+
+(defadvice ido-find-file (after find-file-sudo activate)
+  "Find file as root if necessary."
+  (unless (and buffer-file-name
+               (file-writable-p buffer-file-name))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+
 ;;;;
 ;;;; Smex is a M-x enhancement for Emacs
 ;;;;
