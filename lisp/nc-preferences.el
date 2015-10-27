fb0908a3e57acc76462404f1c135126ae75494f6
Nicolas Chapon <nicolas.chapon@cnp.fr>
2015-10-27 17:32:17 +0100

    Change coding preferences

--- lisp/nc-preferences.el
+++ lisp/nc-preferences.el
@@ -53,12 +53,12 @@
 
 ;; Set all coding systems to utf-8
 (set-language-environment 'utf-8)
-(set-default-coding-systems 'utf-8)
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8-unix)
+(setq locale-coding-system 'utf-8-unix)
+(set-terminal-coding-system 'utf-8-unix)
+(set-keyboard-coding-system 'utf-8-unix)
+(set-selection-coding-system 'utf-8-unix)
+(prefer-coding-system 'utf-8-unix)
 
 ;; Default indentation
 (setq-default indent-tabs-mode nil)
