5e4517c8b871af0b2106582b5171e45482e5eb14
Nicolas Chapon <nicolas.chapon@cnp.fr>
2015-11-05 17:32:36 +0100

    List archiving tasks

--- lisp/nc-org.el
+++ lisp/nc-org.el
@@ -39,6 +39,28 @@
 ;; Line in agenda is selected
 (add-hook 'org-agenda-mode-hook 'hl-line-mode)
 
+(defun nc/skip-non-archivable-tasks ()
+  "Skip trees that are not available for archiving"
+  (save-restriction
+    (widen)
+    ;; Consider only tasks with done todo headings as archivable candidates
+    (let ((next-headline (save-excursion (or (outline-next-heading) (point-max))))
+          (subtree-end (save-excursion (org-end-of-subtree t))))
+      (if (member (org-get-todo-state) org-todo-keywords-1)
+          (if (member (org-get-todo-state) org-done-keywords)
+              (let* ((daynr (string-to-int (format-time-string "%d" (current-time))))
+                     (a-month-ago (* 60 60 24 (+ daynr 1)))
+                     (last-month (format-time-string "%Y-%m-" (time-subtract (current-time) (seconds-to-time a-month-ago))))
+                     (this-month (format-time-string "%Y-%m-" (current-time)))
+                     (subtree-is-current (save-excursion
+                                           (forward-line 1)
+                                           (and (< (point) subtree-end)
+                                                (re-search-forward (concat last-month "\\|" this-month) subtree-end t)))))
+                (if subtree-is-current
+                    subtree-end ; Has a date in this month or last month, skip it
+                  nil))  ; available to archive
+            (or subtree-end (point-max)))
+        next-headline))))
 
 (setq org-agenda-custom-commands
       '(("P" "Projects"
@@ -49,7 +71,12 @@
           (tags-todo "CNP")
           (tags-todo "HOME")
           (tags-todo "COMPUTER")
-          (tags-todo "READING")))
+          (tags-todo "READING")
+          (tags "-REFILE"
+                ((org-agenda-overriding-header "Tasks to Archive")
+                       (org-agenda-skip-function 'nc/skip-non-archivable-tasks)
+                       (org-tags-match-list-sublevels nil)))
+          ))
 
         ("D" "Daily Action List"
          ((agenda "" ((org-agenda-ndays 1)
