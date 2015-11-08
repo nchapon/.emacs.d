885481b48199e225d897bd32e0ca3f5a1df5d051
Nicolas Chapon <nchapon@gmail.com>
2015-11-08 22:39:28 +0100

    Add journal in agenda files

--- custom/xps13/init.el
+++ custom/xps13/init.el
@@ -2,6 +2,7 @@
 
 (setq org-agenda-files (list (nc/expand-org-notes-path "GTD/todo.org")
                              (nc/expand-org-notes-path "GTD/refile.org")
+                             (nc/expand-org-notes-path "GTD/journal.org")
                              (nc/expand-org-notes-path "GTD/someday.org")))
 
 ;;; Org crypt to use GPG
