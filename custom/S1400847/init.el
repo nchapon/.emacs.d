ffdf4f816468a31cb49dc59b68b9fa3594e7c441
Nicolas Chapon <nicolas.chapon@cnp.fr>
2015-12-21 18:43:12 +0100

    Add office folder in agenda files

--- custom/S1400847/init.el
+++ custom/S1400847/init.el
@@ -44,6 +44,7 @@
 
 ;;; Agenda configuration
 (setq org-agenda-files (list (nc/expand-org-notes-path "GTD/office/")
+                             (nc/expand-org-notes-path "GTD/office/cnp/")
                              (nc/expand-org-notes-path "GTD/refile.org")
                              (nc/expand-org-notes-path "GTD/journal.org")
                              (nc/expand-org-notes-path "GTD/weekly-review.org")))
