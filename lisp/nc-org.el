6fa1794a638fc20e31e4185949f3909d23fde85b
Nicolas Chapon <nicolas.chapon@cnp.fr>
2016-03-24 17:00:12 +0100

    Configure habits

--- lisp/nc-org.el
+++ lisp/nc-org.el
@@ -5,6 +5,16 @@
 ;; Automatically resetting the check boxes to unchecked when marking the repeated task DONE
 (require 'org-checklist)
 
+(require 'org-install)
+(require 'org-habit)
+(add-to-list 'org-modules "org-habit")
+(setq org-habit-preceding-days 7
+      org-habit-following-days 1
+      org-habit-graph-column 80
+      org-habit-show-habits-only-for-today t
+      org-habit-show-all-today t)
+
+
 ;;; Start agenda on current day
 (setq org-agenda-start-on-weekday nil)
 (setq org-agenda-skip-deadline-if-done t)
@@ -12,7 +22,6 @@
 
 ;; Display deadlines 10
 (setq org-deadline-warning-days 10)
-
 
 (setq org-todo-keywords
       (quote ((sequence "TODO(t)" "NEXT(n)" "SOMEDAY(s)" "|" "DONE(d)")
