74b443904f824a9f7f68668ae8d7a683be8480b4
Nicolas Chapon <nchapon@gmail.com>
2016-04-26 15:14:23 +0200

    Ignore global directories

--- lisp/nc-projectile.el
+++ lisp/nc-projectile.el
@@ -8,5 +8,10 @@
 (setq projectile-switch-project-action (quote helm-projectile))
 
 
+(add-to-list 'projectile-globally-ignored-directories "elpa")
+(add-to-list 'projectile-globally-ignored-directories ".cache")
+(add-to-list 'projectile-globally-ignored-directories "node_modules")
+(add-to-list 'projectile-globally-ignored-directories "image-dired")
+
 
 (provide 'nc-projectile)
