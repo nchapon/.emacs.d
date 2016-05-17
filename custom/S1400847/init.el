89ae33f3f20fc0246a7e2d38c0586b0988365102
Nicolas Chapon <nchapon@gmail.com>
2016-05-17 22:36:58 +0200

    Fix magit problems on windows

--- custom/S1400847/init.el
+++ custom/S1400847/init.el
@@ -1,25 +1,6 @@
 (require 'use-package)
-;;; Windows with cygwin config
-(let* ((cygwin-root "C:\\PROGRAMJAVA\\tools\\.babun\\cygwin")
-         (cygwin-bin (concat cygwin-root "\\bin")))
-    (when (and (eq 'windows-nt system-type)
-         (file-readable-p cygwin-root))
 
-      (setq exec-path (cons cygwin-bin exec-path))
-      (setenv "PATH" (concat cygwin-bin ";" (getenv "PATH")))
 
-      ;; By default use the Windows HOME.
-      ;; Otherwise, uncomment below to set a HOME
-      ;;      (setenv "HOME" (concat cygwin-root "/home/eric"))
-
-      ;; NT-emacs assumes a Windows shell. Change to bash.
-      (setq shell-file-name "zsh")
-      (setenv "SHELL" shell-file-name)
-      (setq explicit-shell-file-name shell-file-name)
-
-      ;; This removes unsightly ^M characters that would otherwise
-      ;; appear in the output of java applications.
-      (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)))
 
 
 ;;; Force projectile to use of external indexing in Windows
