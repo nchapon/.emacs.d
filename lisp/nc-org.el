5e308ed482b46656613f59ae4c6e3a02a8d6e199
Nicolas Chapon <nicolas.chapon@cnp.fr>
2016-05-24 09:39:31 +0200

    Add org time report day by day

--- lisp/nc-org.el
+++ lisp/nc-org.el
@@ -249,6 +249,39 @@
         ))
 
 
+
+;;; Org time report day by day
+(defun org-dblock-write:rangereport (params)
+  "Display day-by-day time reports."
+  (let* ((ts (plist-get params :tstart))
+         (te (plist-get params :tend))
+         (start (time-to-seconds
+                 (apply 'encode-time (org-parse-time-string ts))))
+         (end (time-to-seconds
+               (apply 'encode-time (org-parse-time-string te))))
+         day-numbers)
+    (setq params (plist-put params :tstart nil))
+    (setq params (plist-put params :end nil))
+    (while (<= start end)
+      (save-excursion
+        (insert "\n\n"
+                (format-time-string (car org-time-stamp-formats)
+                                    (seconds-to-time start))
+                "----------------\n")
+        (org-dblock-write:clocktable
+         (plist-put
+          (plist-put
+           params
+           :tstart
+           (format-time-string (car org-time-stamp-formats)
+                               (seconds-to-time start)))
+          :tend
+          (format-time-string (car org-time-stamp-formats)
+                              (seconds-to-time end))))
+        (setq start (+ 86400 start))))))
+
+
+
 ;; Refile
 ;; Targets include this file and any file contributing to the agenda - up to 2 levels deep
 (setq org-reverse-note-order t)
