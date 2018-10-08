035028df60f12676d0f67cab2ba487e7d0fa226b
nchapon <nchapon@gmail.com>
2018-10-08 22:34:47 +0200

    Optimize Init.el

--- init.el
+++ init.el
@@ -1,14 +1,12 @@
+;; Make startup faster by reducing the frequency of garbage
+;; collection.
+(setq gc-cons-threshold (* 50 1000 1000))
+
 (require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
-
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-	(package-refresh-contents)
-	(package-install 'use-package))
-
 
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/nchapon.org"))
+
+;; Make gc pauses faster by decreasing the threshold.
+(setq gc-cons-threshold (* 2 1000 1000))
