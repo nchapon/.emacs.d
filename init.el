c67cd5f643d22ec821bb75421c9fde5cf92a03d3
nchapon <nchapon@gmail.com>
2021-02-10 15:55:06 +0100

    Improve Performance Startup
     - Remove use-package-refresh-contents
     - use-pacxkage optimizations

--- init.el
+++ init.el
@@ -1,13 +1,8 @@
-;; Make startup faster by reducing the frequency of garbage
-;; collection.
-(setq gc-cons-threshold (* 50 1000 1000))
-
 (require 'package)
 (package-initialize)
-
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 800 1000 1000))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/nchapon.org"))
 
-;; Make gc pauses faster by decreasing the threshold.
-(setq gc-cons-threshold (* 2 1000 1000))
 (put 'upcase-region 'disabled nil)
