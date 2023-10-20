fd3192eb44a477535c90b7f53702503b51cdcdc9
nchapon <nicolas.chapon@unseenlabs.fr>
2023-10-20 18:00:00 +0200

    Change treemacs keybinding

--- etc/abbrev.el
+++ etc/abbrev.el
@@ -1,13 +1,11 @@
 ;;-*-coding: utf-8;-*-
 (define-abbrev-table 'org-mode-abbrev-table
   '(
-    ("11" "One To One" "arch" :count 2)
+    ("11" "One To One" nil :count 2)
     ("cb" "[/]" nil :count 2)
-    ("dm" "Daily Meeting" nil :count 0)
-    ("dom" "domaine" nil :count 0)
-    ("fc" "Fondation Cloud" nil :count 0)
-    ("mo" "monolithe" nil :count 4)
-    ("ops" "DevOps Sync" nil :count 1)
-    ("pha" "Point Hebdo Architecture" nil :count 1)
+    ("_10h" "Lundi 10h" nil :count 0)
+    
+    ("_ops" "DevOps Sync" nil :count 1)
+    ("_ssp" "Weekly SSP" nil :count 1)
    ))
 
